021e98a1bc3f4d13749e31297a33cef21aac816d
ebubekirmangal <mangalbekir1880@gmail.com>
2023-07-23 01:58:04 +0300

    22

--- responsİnfo.js
+++ responsİnfo.js
@@ -3,16 +3,19 @@
 const contact = document.querySelector(".contact")
 const cardLisans = document.querySelector(".cardLisans")
 const iletişimBilgileri = document.querySelector(".tt").addEventListener("click",getInfo)
+const caption = document.querySelector(".caption")
+const body = document.getElementsByTagName("body")
 
 function getInfo(){
 if(contact.classList.contains("active")==false){
   cardLisans.classList.add("active")
   contact.classList.add("active")
-  console.log("tuşa basıldı")
-
+  caption.classList.add("active")
+  body.classList.add("active")
 }
 else{
   cardLisans.classList.remove("active")
   contact.classList.remove("active")
+  caption.classList.remove("active")
 }
 }
