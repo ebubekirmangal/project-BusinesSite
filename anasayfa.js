42e6c137ba5d5b4b2b27bc31455b423e0402ce59
ebubekirmangal <mangalbekir1880@gmail.com>
2023-07-26 00:02:58 +0300

    menü de gelişme ve ana sayfaya eklenti

--- anasayfa.js
+++ anasayfa.js
@@ -1,31 +1,14 @@
-const images = ["section img/2.png", "section img/1.png", "section img/3.png", "section img/4.png"];
-let currentImageIndex = 0;
+const imgs = ["section img/2.png", "section img//1.png", "section img//3.png", "section img//4.png"];
+let currentIndex = 0;
 
-function showImage(index) {
-  const imageElement = document.getElementById("imgSec");
-  imageElement.src = images[index];
-}
-
-function prevImage() {
-  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-  showImage(currentImageIndex);
-}
-
-function nextImage() {
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  showImage(currentImageIndex);
-}
-
-function startSlideshow() {
-  
-    setInterval(() => {
-      nextImage();
-    }, 4000); 
+function showImg() {
+  const displayedImage = document.getElementById("imgSec");
+  displayedImage.setAttribute("src", imgs[currentIndex]);
+  currentIndex++;
+  if (currentIndex === imgs.length) {
+    currentIndex = 0;
   }
-  
+}
 
-  showImage(currentImageIndex);
-  
-
-  startSlideshow();
+setInterval(showImg, 1500);
 
